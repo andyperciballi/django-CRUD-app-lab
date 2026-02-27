b248472b1db0ca8eb4d90fc644ed7ac8c5b58898
andrew-perc <andyperciballi@gmail.com>
2026-02-27 15:10:39 -0500

    completed lab

--- django_crud_app_mobile_wallpapers/main_app/models.py
+++ django_crud_app_mobile_wallpapers/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -16,6 +17,7 @@ class Wallpaper(models.Model):
     source = models.CharField(max_length=200)
     creator = models.CharField(max_length=100)
     tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Wallpaper by {self.creator} from {self.source}"
--- django_crud_app_mobile_wallpapers/main_app/views.py
+++ django_crud_app_mobile_wallpapers/main_app/views.py
@@ -4,13 +4,19 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .forms import UseLogForm
 from django.urls import reverse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return render(request, 'home.html')
+class Home(LoginView):
+    template_name = 'home.html'
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def wallpaper_detail(request, pk):
     wallpaper = Wallpaper.objects.get(id=pk)
     use_log_form = UseLogForm()
@@ -21,6 +27,7 @@ def wallpaper_detail(request, pk):
         'tags': tags
     })
 
+@login_required
 def add_use_log(request, wallpaper_id):
     form = UseLogForm(request.POST)
     if form.is_valid():
@@ -29,42 +36,63 @@ def add_use_log(request, wallpaper_id):
         new_log.save()
     return redirect('wallpaper-detail', pk=wallpaper_id)
 
+@login_required
 def associate_tag(request, wallpaper_id, tag_id):
     Wallpaper.objects.get(id=wallpaper_id).tags.add(tag_id)
     return redirect('wallpaper-detail', pk=wallpaper_id)
 
-class WallpaperList(ListView):
+class WallpaperList(LoginRequiredMixin, ListView):
     model = Wallpaper
     template_name = 'wallpapers/index.html'
 
-class WallpaperCreate(CreateView):
+class WallpaperCreate(LoginRequiredMixin, CreateView):
     model = Wallpaper
     fields = ['image_url', 'source', 'creator']
 
-class WallpaperUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class WallpaperUpdate(LoginRequiredMixin, UpdateView):
     model = Wallpaper
     fields = ['image_url', 'source', 'creator']
 
-class WallpaperDelete(DeleteView):
+class WallpaperDelete(LoginRequiredMixin, DeleteView):
     model = Wallpaper
     success_url = '/wallpapers'
 
-class TagCreate(CreateView):
+class TagCreate(LoginRequiredMixin, CreateView):
     model = Tag
     fields = '__all__'
 
-class TagDetail(DetailView):
+class TagDetail(LoginRequiredMixin, DetailView):
     model = Tag
     template_name = 'tags/detail.html'
 
-class TagList(ListView):
+class TagList(LoginRequiredMixin, ListView):
     model = Tag
     template_name = 'tags/index.html'
 
-class TagUpdate(UpdateView):
+class TagUpdate(LoginRequiredMixin, UpdateView):
     model = Tag
     fields = ['name', 'color']
 
-class TagDelete(DeleteView):
+class TagDelete(LoginRequiredMixin, DeleteView):
     model = Tag
     success_url = '/tags/'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('wallpaper-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    return render(request, 'signup.html', {
+        'form': form,
+        'error_message': error_message
+    })
